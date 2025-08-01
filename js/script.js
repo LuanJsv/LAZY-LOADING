1e035a2dc89d7338667ad717b1bc3bd568a196cc
LuanJsv <luan.o.dantas@gmail.com>
2025-07-31 21:38:00 -0300

    Script finalizado

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const imagens = document.querySelectorAll('.image-container img');
+
+const observer = new IntersectionObserver((entries, observer)=>{
+
+    entries.forEach(entry =>{
+
+        if(!entry.isIntersecting) return
+
+        const image = entry.target
+
+        image.src = image.src.replace('&w=10&','&w=1200')
+
+        observer.unobserve(image)
+    })
+
+}, {})
+
+imagens.forEach((image)=>{
+    observer.observe(image)
+})
+
+
+
+
+
